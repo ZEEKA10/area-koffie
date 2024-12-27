ac9040ec249d936ee5968c9a0c04824947399cdd
ZEEKA10 <rizkaputririandra@gmail.com>
2024-12-28 01:44:01 +0800

    Tambahkan Database

---
+++ logic/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+     $hostname = "localhost";
+     $username = "root";
+     $password = "";
+     $database = "area_koffie";
+
+     $conn  = mysqli_connect(
+               $hostname,
+               $username,
+               $password,
+               $database);
+
+?>
