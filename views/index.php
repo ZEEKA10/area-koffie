bbe4a6b2c2d988e2f0936addff27907ce875c2ac
kirananjake <kiranaalopjake@gmail.com>
2024-12-28 11:00:06 +0800

    add view in menu

--- views/index.php
+++ views/index.php
@@ -12,15 +12,15 @@
         <ul class="flex space-x-4">
             <li><a href="#" @click.prevent="currentPage = 'dashboard'" class="text-white hover:underline">Dashboard</a>
             </li>
-            <li><a href="#" @click.prevent="currentPage = 'menu'" class="text-white hover:underline">Menu</a></li>
+            <li><a href="../views/menu.php" @click.prevent="currentPage = 'menu'" class="text-white hover:underline">Menu</a></li>
             <li><a href="#" @click.prevent="currentPage = 'categories'"
                     class="text-white hover:underline">Categories</a></li>
     </nav>
 
     <main class="flex-1 p-4">
-        <div x-show="currentPage === 'dashboard'" x-html="await (await fetch('dashboard.html')).text()"></div>
-        <div x-show="currentPage === 'menu'" x-html="await (await fetch('menu.html')).text()"></div>
-        <div x-show="currentPage === 'categories'" x-html="await (await fetch('category.html')).text()"></div>
+        <div x-show="currentPage === 'dashboard'" x-html="await (await fetch('dashboard')).text()"></div>
+        <div x-show="currentPage === 'menu'" x-html="await (await fetch('views/menu.php')).text()"></div>
+        <div x-show="currentPage === 'categories'" x-html="await (await fetch('category.php')).text()"></div>
     </main>
 
 </body>
